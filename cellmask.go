1835187eff0d668fad2d201a1102dd4b3040ce46
Matthew Johnson <johnsonma@tesla.com>
2020-05-07 13:00:12 -0700

    Add cell mapping and write cell mask to FXR
    Add cell mapping to configuration and create the bitmasks for each half
    of the tray to send to the FXR.
    
    TF-707

---
+++ cellmask.go
@@ -0,0 +1,15 @@
+package towercontroller
+
+func newCellMask(present []bool) []uint32 {
+	cm := make([]uint32, len(present)/32)
+
+	for i, cell := range present {
+		if !cell {
+			continue
+		}
+
+		cm[i/32] |= 1 << i % 32
+	}
+
+	return cm
+}
---
+++ cellmask_test.go
@@ -0,0 +1,43 @@
+package towercontroller
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// common in tests
+// nolint: scopelint
+func Test_newCellMask(t *testing.T) {
+	testCases := []struct {
+		cps []bool
+		exp []uint32
+	}{
+		{
+			cps: []bool{
+				true, true, false, true, true, false, false, false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
+				false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, true,
+			},
+			exp: []uint32{0x1b, 0x0},
+		},
+		{
+			cps: []bool{},
+			exp: []uint32{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			act := newCellMask(tc.cps)
+			if !assert.Equal(t, len(tc.exp), len(act)) {
+				return
+			}
+
+			for i, a := range act {
+				assert.Equal(t, tc.exp[i], a)
+			}
+		})
+	}
+}
