981b4b8734ea10c3dd1061c530a1bd9908cc405f
Ken Steinfeldt <ksteinfeldt@tesla.com>
2020-08-18 08:49:04 -0700

    Set deployment configuration for mass pro
    Docker image is changed to point to artifactory repository and configurations
    are changed to no longer depend on a local git repository being present.
    
    TF-1203 TF-1253

--- canup.sh
+++ canup.sh
@@ -8,7 +8,7 @@ ip link set can0 txqueuelen 1000
 ip link set can1 up
 ip link set can1 up type can bitrate 500000 dbitrate 2000000 dsample-point 0.75 fd on
 ip link set can1 txqueuelen 1000
-pushd /home/mfgtest/projects/can-isotp
+pushd /opt/canup/can-isotp
 make
 make modules_install
 insmod ./net/can/can-isotp.ko 
