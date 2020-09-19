64a3389e4b4070506af96a8dda655957288670f6
Ken Steinfeldt <ksteinfeldt@tesla.com>
2020-09-18 23:36:14 -0700

    Add 'make clean' to canup.sh

--- canup.sh
+++ canup.sh
@@ -9,6 +9,7 @@ ip link set can1 up
 ip link set can1 up type can bitrate 500000 dbitrate 2000000 dsample-point 0.75 fd on
 ip link set can1 txqueuelen 1000
 pushd /opt/canup/can-isotp
+make clean
 make
 make modules_install
 insmod ./net/can/can-isotp.ko 
