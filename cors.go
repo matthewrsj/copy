17e2f280e59a05de5618c92cb25f6cf09e808b03
Drew Slobodnjak <dslobodnjak@tesla.com>
2020-12-22 17:36:01 -0800

    Add OPTIONS to cors methods

--- cors.go
+++ cors.go
@@ -5,6 +5,6 @@ import "net/http"
 func allowCORS(w http.ResponseWriter) {
 	// TODO: more fine-grained control
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
